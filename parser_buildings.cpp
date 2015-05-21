fe5a779067d59c712b445712a8f3143ccfef0744
David Cheung <cheung.david@hotmail.ca>
2015-05-20 21:54:49 -0400

    Added new building parsing files

---
+++ parser_buildings.cpp
@@ -0,0 +1,15 @@
+// Parser - Buildings
+// By David Cheung
+// Performs all data parsing for buildings
+
+#include <iostream>
+
+
+using namespace std;
+
+
+// Main parsing function
+void parseAllBuildings()
+{
+    
+}
