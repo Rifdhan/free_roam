f23a58d2e06913c9fe59f036eafb3dba62a37eed
Rifdhan Nazeer <rifdhan.nazeer@gmail.com>
2015-05-20 21:38:45 -0400

    Added new main parsing files

---
+++ parser_main.cpp
@@ -0,0 +1,16 @@
+// Parser - Main
+// By Rifdhan Nazeer
+// Performs all data parsing for initialization, using smaller parsing sub-functions
+
+
+#include <iostream>
+
+
+using namespace std;
+
+
+// Main parsing function
+void parseAllData()
+{
+    
+}
---
+++ parser_main.h
@@ -0,0 +1,13 @@
+// Parser - Main
+// By Rifdhan Nazeer
+// Performs all data parsing for initialization, using smaller parsing sub-functions
+
+
+#include <iostream>
+
+
+using namespace std;
+
+
+// Main parsing function
+void parseAllData();
