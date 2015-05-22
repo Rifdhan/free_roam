dfc4c8e1320d27d04a421928507d92716795c84e
David Cheung <cheung.david@hotmail.ca>
2015-05-21 22:48:31 -0400

    Created a parser helper function

--- parser_helpers.cpp
+++ parser_helpers.cpp
@@ -13,5 +13,82 @@ using namespace std;
 // By David Cheung
 pair parseKeyValPair(string line)
 {
-    
+        typedef string::const_iterator iter;
+        iter b = s.begin();
+        iter e = s.end();
+        int valueStart, valueEnd = 0;
+        int seperator = 0;        
+        int keyStart, keyEnd = 0;
+        int flag = 0;
+        
+        while (1)
+        {
+        
+                // Attempting to find first letter of key.
+        
+                if (b == '<' || b == ' '){
+                        b++;
+                } else {
+                // key found
+                        keyStart = b;
+                        break;
+                }
+        }
+        
+        
+       
+        
+        while (1)
+        {
+                // Find end of key
+                if (b == '=' || b == ' '){
+                        keyEnd = b - 1;
+                        break;
+                } else {
+                        b++;
+                }
+        }
+        
+        while (1)
+        {
+                // find beginning of value
+                if (b == '=' || b == ' ') b++;
+                break;
+        
+        }
+        
+                // determine whether the value is in quotations
+                if (b == '"'){
+                        valueStart = b + 1;
+                        flag = 1;
+                } else {
+                        valueStart = b;
+                }
+        
+        // find end of value
+        while(b != e)
+        {
+                // find closing quotation (type 1)
+                if (flag == 1){
+                        if (b == '"'){
+                                valueEnd = b - 1;
+                                break;
+                        } else {
+                                b++;
+                        }
+                // no surrounding quotations so find first space or closing bracket.                      
+                } else {
+                        if (b == ">" || b == ' '){
+                                value_end = b - 1;
+                                break;
+                        } else {
+                                b++;
+                        } 
+                }
+        }
+        return make_pair(line.substr(keyStart, keyEnd), 
+                        line.substr(valueStart, valueEnd));        
+               
+                        
+              
 }
