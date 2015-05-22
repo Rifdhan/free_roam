7828652d8f90ac70351a886e368ab92e70dafc7a
Rifdhan Nazeer <rifdhan.nazeer@gmail.com>
2015-05-21 21:19:41 -0400

    Repurposed main parser file into helper functions file

---
+++ parser_helpers.cpp
@@ -0,0 +1,17 @@
+// Parser - Helpers
+// By Rifdhan Nazeer and David Cheung
+// Several useful helper functions for parsing XML data
+
+
+#include "parser_helpers.h"
+
+
+using namespace std;
+
+
+// Parses the given line and returns a key-value pair
+// By David Cheung
+pair parseKeyValPair(string line)
+{
+    
+}
