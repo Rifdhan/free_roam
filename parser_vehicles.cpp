e9ec9f7790601bd0308b0bb135b2a29dc4c6dcfb
David Cheung <cheung.david@hotmail.ca>
2015-07-05 00:11:53 -0400

    Added new vehicles parsing files.

---
+++ parser_vehicles.cpp
@@ -0,0 +1,16 @@
+// Parser - Vehicles
+// By David Cheung
+// Performs all data parsing for vehicles
+
+
+#include "parser_vehicles.h"
+
+
+using namespace std;
+
+
+// Main parsing function
+void parseAllVehicles()
+{
+    
+}
---
+++ parser_vehicles.h
@@ -0,0 +1,10 @@
+// Parser - Vehicles
+// By David Cheung
+// Performs all data parsing for vehicles
+
+
+#include <iostream>
+
+
+// Main parsing function
+void parseAllVehicles();
